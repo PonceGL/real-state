6381fcdbe04036c73cc8f73a1fd3ee2db8c3af67
marcos-gl <marcosgueloz@gmail.com>
2025-10-09 13:04:59 -0600

    feat(page): a new page for public

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,12 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default { 
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-          2,
-          'always',
-          ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test', 'chore'],
-        ],
-        'scope-empty': [2, 'never'],
-      },
- };
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test', 'chore'],
+    ],
+    'scope-empty': [2, 'never'],
+  },
+};
