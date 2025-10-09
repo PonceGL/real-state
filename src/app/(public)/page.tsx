38385862f74dcd44cafa9b0085ab984a3090c6b4
marcos-gl <84688393+marcos-gl@users.noreply.github.com>
2025-10-09 15:40:40 -0600

    feat: a new page for public (#2)
    * feat(page): a new page for public
    
    * fix(page): updating page.tsx
    
    * fix(page): new page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <h1>Bienvenido a Next.js!</h1>
-      <p>Página de inicio para nuestra app.</p>
+      <p>Página de inicio para a app.</p>
     </main>
   );
 }
