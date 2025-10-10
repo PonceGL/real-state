20aedd3f0a4532f9aa8c8b64b757cc1666d2cf3b
marcos-gl <marcosgueloz@gmail.com>
2025-10-10 12:48:13 -0600

    test(Button): height and size of the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
     return (
-        <button>
+        <button className=" h-[37px] w-[362px] rounded-[8px] bg-black  text-amber-50 m-2">
             Click
         </button>
     );
