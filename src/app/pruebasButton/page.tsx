8b7b67231d2c426f44b990911ed4ec3777c50246
marcos-gl <marcosgueloz@gmail.com>
2025-10-17 17:54:51 -0600

    feat(input): input functionality

--- src/app/pruebasButton/page.tsx
+++ src/app/pruebasButton/page.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import { Input } from "@/components/ui/input"
+
 
 
 export default function PruebaButton() {
     return (
         <>
-            <Input type="email" placeholder="email" />
-            <Input type="text" placeholder="Escribe..." />
-            <Input type="tel" placeholder="123..." />
-            <Input type="text" placeholder="123..." />
+
         </>
     )
 }
--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -2,18 +2,24 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+type AllInputType = 'text' | 'email' | 'tel' | 'textarea';
 
+export interface InputProps extends Omit<React.ComponentPropsWithoutRef<'input'>, 'type'> {
+  inputType: AllInputType;
+  borderColorClass?: string;
+}
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({ borderColorClass, className, inputType, ...props }: InputProps) {
   return (
     <input
-      type={type}
+      type={inputType}
       data-slot="input"
       className={cn(
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-blue-500 focus-visible:ring-blue-500/50 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        className,
+        borderColorClass
       )}
       {...props}
     />
