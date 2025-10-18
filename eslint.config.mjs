4599329384ac9b28dc5f79de35f21179d93dfbc2
“PonceGL” <poncianogl@hotmail.com>
2025-10-18 13:55:08 -0600

    feat(eslint): integrate validate-filename plugin and update rules for file naming conventions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
+import validateFileName from "eslint-plugin-validate-filename";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -13,12 +14,84 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    files: ["**/*.{js,jsx,ts,tsx}"],
     plugins: {
       "simple-import-sort": simpleImportSort,
+      "validate-filename": validateFileName,
     },
     rules: {
+      "@next/next/no-html-link-for-pages": "warn",
+      "@next/next/no-img-element": "warn",
       "simple-import-sort/imports": "warn",
       "simple-import-sort/exports": "warn",
+      "validate-filename/naming-rules": [
+        "error",
+        {
+          rules: [
+            {
+              case: 'kebab',
+              target: "**/app/**",
+              excludes: ['dtos'],
+            },
+            {
+              case: 'camel',
+              target: "**/components/**",
+            },
+            {
+              case: 'camel',
+              target: "**/config/**",
+            },
+            {
+              case: 'camel',
+              target: "**/constants/**",
+            },
+            {
+              case: 'camel',
+              target: "**/lib/**",
+            },
+            {
+              case: 'camel',
+              target: "**/types/**",
+            },
+            {
+              case: 'camel',
+              target: "**/hooks/**",
+              patterns: '^use',
+            }
+          ],
+        },
+      ],
+      "validate-filename/limit-extensions": [
+        "error",
+        {
+          rules: [
+            {
+              target: "**/app/**",
+              extensions: ['.ts', '.tsx'],
+            },
+            {
+              target: "**/hooks/**",
+              extensions: ['.ts'],
+            },
+            {
+              target: "**/config/**",
+              extensions: ['.ts'],
+            },
+            {
+              target: "**/constants/**",
+              extensions: ['.ts'],
+            },
+            {
+              target: "**/lib/**",
+              extensions: ['.ts'],
+            },
+            {
+              target: "**/types/**",
+              extensions: ['.ts'],
+            },
+          ],
+        },
+      ],
     },
   },
   {
@@ -37,10 +110,6 @@ const eslintConfig = [
       next: {
         rootDir: ["./", "src/"],
       },
-    },
-    rules: {
-      "@next/next/no-html-link-for-pages": "warn",
-      "@next/next/no-img-element": "warn",
     },
   },
 ];
