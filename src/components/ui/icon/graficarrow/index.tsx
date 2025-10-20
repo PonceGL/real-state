234eae25d02c622a6d0b86edca9905061b5ab63b
marcos-gl <marcosgueloz@gmail.com>
2025-10-20 15:42:34 -0600

    feat(index): graficarrow icon

---
+++ src/components/ui/icon/graficarrow/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const GrafficArrowIcon = ({
+    width = 24,
+    height = 24,
+    color = "black",
+    className = ""
+}) => {
+    return (
+        <svg
+            width={width}
+            height={height}
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+        >
+            <path
+                d="M22 7L13.5 15.5L8.5 10.5L2 17"
+                stroke={color}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path
+                d="M16 7H22V13"
+                stroke={color}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+};
+
+export default GrafficArrowIcon;
