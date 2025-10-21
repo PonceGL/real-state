fffa9f9c21ae7348c4409dd525ca94f70f6312c4
“PonceGL” <poncianogl@hotmail.com>
2025-10-21 11:40:34 -0600

    test(LoginForm): update tests

--- src/components/loginform/loginform.test.tsx
+++ src/components/loginform/loginform.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import React from "react";
 
 import { LoginForm } from "./index";
@@ -9,55 +9,58 @@ beforeEach(() => {
 
 describe("LoginForm", () => {
   test("renders all form fields and submit button", () => {
-    render(<LoginForm />);
-    expect(screen.getByLabelText(/Correo Electrónico/i)).toBeInTheDocument();
-    expect(screen.getByLabelText(/Contraseña/i)).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /Enviar/i })).toBeInTheDocument();
+    const { getByLabelText, getByRole } = render(<LoginForm />);
+    expect(getByLabelText(/Correo Electrónico/i)).toBeInTheDocument();
+    expect(getByLabelText(/Contraseña/i)).toBeInTheDocument();
+    expect(getByRole("button", { name: /Enviar/i })).toBeInTheDocument();
   });
 
   test("shows error for short password", async () => {
-    render(<LoginForm />);
-    fireEvent.change(screen.getByLabelText(/Correo Electrónico/i), {
+    const { getByLabelText, getByRole, getByText } = render(<LoginForm />);
+    fireEvent.change(getByLabelText(/Correo Electrónico/i), {
       target: { value: "test@example.com" },
     });
-    fireEvent.change(screen.getByLabelText(/Contraseña/i), {
+    fireEvent.change(getByLabelText(/Contraseña/i), {
       target: { value: "123" },
     });
-    fireEvent.click(screen.getByRole("button", { name: /Enviar/i }));
+    fireEvent.click(getByRole("button", { name: /Enviar/i }));
     await waitFor(() => {
       expect(
-        screen.getByText(/La contraseña debe tener al menos 6 caracteres./i)
+        getByText(/La contraseña debe tener al menos 6 caracteres./i)
       ).toBeInTheDocument();
     });
   });
 
   test("shows error for long password", async () => {
-    render(<LoginForm />);
-    fireEvent.change(screen.getByLabelText(/Correo Electrónico/i), {
+    const { getByLabelText, getByRole, getByText } = render(
+      <LoginForm isInDarkBackground />
+    );
+    fireEvent.change(getByLabelText(/Correo Electrónico/i), {
       target: { value: "test@example.com" },
     });
-    fireEvent.change(screen.getByLabelText(/Contraseña/i), {
+    fireEvent.change(getByLabelText(/Contraseña/i), {
       target: { value: "a".repeat(51) },
     });
-    fireEvent.click(screen.getByRole("button", { name: /Enviar/i }));
+    fireEvent.click(getByRole("button", { name: /Enviar/i }));
     await waitFor(() => {
       expect(
-        screen.getByText(/La contraseña no puede tener más de 50 caracteres./i)
+        getByText(/La contraseña no puede tener más de 50 caracteres./i)
       ).toBeInTheDocument();
     });
   });
 
+  // TODO: improve this test
   test("submits form with valid data", async () => {
-    render(<LoginForm />);
-    fireEvent.change(screen.getByLabelText(/Correo Electrónico/i), {
+    const { getByLabelText, getByRole } = render(<LoginForm />);
+    fireEvent.change(getByLabelText(/Correo Electrónico/i), {
       target: { value: "user@example.com" },
     });
-    fireEvent.change(screen.getByLabelText(/Contraseña/i), {
+    fireEvent.change(getByLabelText(/Contraseña/i), {
       target: { value: "securePassword" },
     });
-    fireEvent.click(screen.getByRole("button", { name: /Enviar/i }));
+    fireEvent.click(getByRole("button", { name: /Enviar/i }));
     await waitFor(() => {
-      expect(console.log).toHaveBeenCalledWith({ // TODO: improve this test
+      expect(console.log).toHaveBeenCalledWith({
         username: "user@example.com",
         password: "securePassword",
       });
