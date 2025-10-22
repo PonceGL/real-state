0064c0b7fafefaf1917909e05711b175f0f2bcba
marcos-gl <marcosgueloz@gmail.com>
2025-10-22 13:39:09 -0600

    feat(index): mountain icon

--- src/components/ui/icon/mountain/index.tsx
+++ src/components/ui/icon/mountain/index.tsx
@@ -1,26 +1,23 @@
+import { iconVariants } from "@/lib/styles/iconVariants";
 import { cn } from "@/lib/styles/utils";
 import { IconProps } from "@/types/iconsinterface";
-import { iconVariants } from "@/types/iconvariants";
 
-const MountainIcon = ({
-    size,
-    variant,
-}: IconProps) => {
-    return (
-        <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className={cn(iconVariants({ size, variant }))}
-        >
-            <path
-                d="M8 3L12 11L17 6L22 21H2L8 3Z"
-                stroke={variant}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-        </svg>
-    );
+const MountainIcon = ({ size, variant }: IconProps) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn(iconVariants({ size, variant }))}
+    >
+      <path
+        d="M8 3L12 11L17 6L22 21H2L8 3Z"
+        stroke={variant}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 };
 
 export default MountainIcon;
