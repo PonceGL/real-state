71f1da340231b829ba9a73f95ec7c225750e3e5e
marcos-gl <marcosgueloz@gmail.com>
2025-10-22 17:08:06 -0600

    feat(index): calculadora icon

--- src/components/ui/icon/calculadora/index.tsx
+++ src/components/ui/icon/calculadora/index.tsx
@@ -1,27 +1,24 @@
-export const CalculatorIcon = ({
-  width = 24,
-  height = 24,
-  color = "#1C2434",
-  className = "",
-}) => {
+import { iconVariants } from "@/lib/styles/iconVariants";
+import { cn } from "@/lib/styles/utils";
+import { IconProps } from "@/types/icons";
+
+export const CalculatorIcon = ({ size, variant }: IconProps) => {
   return (
     <svg
-      width={width}
-      height={height}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={className}
+      className={cn(iconVariants({ size, variant }))}
     >
       <path
         d="M18 2H6C4.89543 2 4 2.89543 4 4V20C4 21.1046 4.89543 22 6 22H18C19.1046 22 20 21.1046 20 20V4C20 2.89543 19.1046 2 18 2Z"
-        stroke={color}
+        stroke="Color"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M8 6H16M16 14V18M16 10H16.01M12 10H12.01M8 10H8.01M12 14H12.01M8 14H8.01M12 18H12.01M8 18H8.01"
-        stroke={color}
+        stroke="Color"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
