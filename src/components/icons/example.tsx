441e6b81bc017bf014485d6baedff05904036c08
marcos-gl <marcosgueloz@gmail.com>
2025-10-23 16:50:32 -0600

    feat(icon): update icons

--- src/components/icons/example.tsx
+++ src/components/icons/example.tsx
@@ -28,6 +28,8 @@ import {
   WhatsAppIcon,
 } from "@/components/icons/index";
 
+import { Icon } from "../ui/icon";
+
 export function ExampleIcons() {
   return (
     <>
@@ -59,6 +61,35 @@ export function ExampleIcons() {
         <ShieldIcon />
         <SquareIcon />
         <TreeIcon />
+      </div>
+      <div className="w-full flex flex-row">
+        <Icon name="BuildingIcon" variant="danger" />
+        <Icon name="CalculatorIcon" variant="danger" />
+        <Icon name="CheckCircleIcon" variant="danger" />
+        <Icon name="ChevronRightIcon" variant="danger" />
+        <Icon name="ClockIcon" variant="danger" />
+        <Icon name="CloseIcon" variant="danger" />
+        <Icon name="DocsIcon" variant="danger" />
+        <Icon name="EmailIcon" variant="danger" />
+        <Icon name="EyeIcon" variant="danger" />
+        <Icon name="FacebookIcon" variant="danger" />
+        <Icon name="FilterIcon" variant="danger" />
+        <Icon name="GraphicArrowIcon" variant="danger" />
+        <Icon name="HandHeartIcon" variant="danger" />
+        <Icon name="HomeIcon" variant="danger" />
+        <Icon name="LocationIcon" variant="danger" />
+        <Icon name="MedalIcon" variant="danger" />
+        <Icon name="MenuIcon" variant="danger" />
+        <Icon name="MountainIcon" variant="danger" />
+        <Icon name="PencilIcon" variant="danger" />
+        <Icon name="PhoneIcon" variant="danger" />
+        <Icon name="SearchIcon" variant="danger" />
+        <Icon name="SendIcon" variant="danger" />
+        <Icon name="ShieldIcon" variant="danger" />
+        <Icon name="SquareIcon" variant="danger" />
+        <Icon name="TreeIcon" variant="danger" />
+        <Icon name="UsersIcon" variant="danger" />
+        <Icon name="WhatsAppIcon" variant="danger" />
       </div>
     </>
   );
--- src/components/ui/icon/index.tsx
+++ src/components/ui/icon/index.tsx
@@ -1,9 +1,72 @@
-export function Icon() {
+import {
+  BuildingIcon,
+  CalculatorIcon,
+  CheckCircleIcon,
+  ChevronRightIcon,
+  ClockIcon,
+  CloseIcon,
+  DocsIcon,
+  EmailIcon,
+  EyeIcon,
+  FacebookIcon,
+  FilterIcon,
+  GraphicArrowIcon,
+  HandHeartIcon,
+  HomeIcon,
+  LocationIcon,
+  MedalIcon,
+  MenuIcon,
+  MountainIcon,
+  PencilIcon,
+  PhoneIcon,
+  SearchIcon,
+  SendIcon,
+  ShieldIcon,
+  SquareIcon,
+  TreeIcon,
+  UsersIcon,
+  WhatsAppIcon,
+} from "@/components/icons/index";
+import { IconProps } from "@/types/icons";
 
-    return(
-        <>
-            <h1>Aquí va el componente Icon</h1>
-        </>
+const iconMap = {
+  BuildingIcon: BuildingIcon,
+  CalculatorIcon: CalculatorIcon,
+  CheckCircleIcon: CheckCircleIcon,
+  ChevronRightIcon: ChevronRightIcon,
+  ClockIcon: ClockIcon,
+  CloseIcon: CloseIcon,
+  DocsIcon: DocsIcon,
+  EmailIcon: EmailIcon,
+  EyeIcon: EyeIcon,
+  FacebookIcon: FacebookIcon,
+  FilterIcon: FilterIcon,
+  GraphicArrowIcon: GraphicArrowIcon,
+  HandHeartIcon: HandHeartIcon,
+  HomeIcon: HomeIcon,
+  LocationIcon: LocationIcon,
+  MedalIcon: MedalIcon,
+  MenuIcon: MenuIcon,
+  MountainIcon: MountainIcon,
+  PencilIcon: PencilIcon,
+  PhoneIcon: PhoneIcon,
+  SearchIcon: SearchIcon,
+  SendIcon: SendIcon,
+  ShieldIcon: ShieldIcon,
+  SquareIcon: SquareIcon,
+  TreeIcon: TreeIcon,
+  UsersIcon: UsersIcon,
+  WhatsAppIcon: WhatsAppIcon,
+};
 
-    )
+type IconName = keyof typeof iconMap;
+
+interface NameIcons extends IconProps {
+  name: IconName;
+}
+
+export function Icon({ name, ...rest }: NameIcons) {
+  const SelectdIcon = iconMap[name];
+
+  return <SelectdIcon {...rest} />;
 }
