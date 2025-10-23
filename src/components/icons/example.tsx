c37589645cb6d9635e64f23fb54f06cc3ecac551
marcos-gl <marcosgueloz@gmail.com>
2025-10-23 16:10:37 -0600

    fix(icon): update icons

--- src/components/icons/example.tsx
+++ src/components/icons/example.tsx
@@ -1,18 +1,37 @@
 import {
   BuildingIcon,
   CalculatorIcon,
+  CheckCircleIcon,
+  ChevronRightIcon,
+  ClockIcon,
+  CloseIcon,
+  DocsIcon,
+  EmailIcon,
+  EyeIcon,
   FacebookIcon,
+  FilterIcon,
+  GraphicArrowIcon,
+  HandHeartIcon,
+  HomeIcon,
+  LocationIcon,
+  MedalIcon,
   MenuIcon,
   MountainIcon,
   PencilIcon,
+  PhoneIcon,
   SearchIcon,
+  SendIcon,
+  ShieldIcon,
+  SquareIcon,
+  TreeIcon,
+  UsersIcon,
   WhatsAppIcon,
 } from "@/components/icons/index";
 
 export function ExampleIcons() {
   return (
     <>
-      <div className="w-full flex flex-row">
+      <div className="w-full flex flex-row text-fuchsia-500">
         <FacebookIcon />
         <BuildingIcon />
         <CalculatorIcon />
@@ -21,6 +40,25 @@ export function ExampleIcons() {
         <MountainIcon />
         <MenuIcon />
         <WhatsAppIcon />
+        <UsersIcon />
+        <CheckCircleIcon />
+        <ChevronRightIcon />
+        <ClockIcon />
+        <CloseIcon />
+        <DocsIcon />
+        <EmailIcon />
+        <EyeIcon />
+        <FilterIcon />
+        <GraphicArrowIcon />
+        <HandHeartIcon />
+        <HomeIcon />
+        <LocationIcon />
+        <MedalIcon />
+        <PhoneIcon />
+        <SendIcon />
+        <ShieldIcon />
+        <SquareIcon />
+        <TreeIcon />
       </div>
     </>
   );
