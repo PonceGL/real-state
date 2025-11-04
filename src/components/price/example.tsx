b44f02ace9f7520f810f0941fabb9a3d0ecd6ade
marcos-gl <marcosgueloz@gmail.com>
2025-11-04 14:11:36 -0600

    test(price): add currency type

--- src/components/price/example.tsx
+++ src/components/price/example.tsx
@@ -1,5 +1,11 @@
 import { Price } from "@/components/price";
 
 export function ExamplePrice() {
-  return <Price number={324568798154} />;
+  return (
+    <>
+      <Price number={324568798154} />
+      <Price number={654812038790} isUSD />
+      <Price number={987456251354.24} />
+    </>
+  );
 }
--- src/components/price/index.tsx
+++ src/components/price/index.tsx
@@ -1,13 +1,21 @@
 interface PriceProps {
-    number: number;
+  number: number;
+  isUSD?: boolean;
 }
 
-export function Price({ number }: PriceProps) {
+export function Price({ number, isUSD = false }: PriceProps) {
+  const currencyText: string = isUSD ? "USD" : "MXN";
+  const locale = isUSD ? "en-US" : "es-MX";
+  const currencySymbol = isUSD ? "USD" : "MXN";
 
-    const formattedNumber = number.toLocaleString("es-MX", {
-        style: "currency",
-        currency: "MXN",
-    });
+  const formattedNumber = number.toLocaleString(locale, {
+    style: "currency",
+    currency: currencySymbol,
+  });
 
-  return <h1>{formattedNumber}</h1>;
+  return (
+    <h1>
+      {currencyText} {formattedNumber}
+    </h1>
+  );
 }
