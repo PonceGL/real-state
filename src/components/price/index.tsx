ebb9b63010b9d05eb04105afa460725ec7bc7de5
marcos-gl <marcosgueloz@gmail.com>
2025-11-06 12:48:25 -0600

    docs(price): documentation was added to the function

--- src/components/price/index.tsx
+++ src/components/price/index.tsx
@@ -7,6 +7,24 @@ export interface PriceProp {
 export type currencyType = "USD" | "MXN" | "EUR";
 export type countryType = "es-US" | "es-MX" | "de-DE";
 
+
+/**
+ * @component
+ * @param {PriceProp} props - value - currenType - countrysType.
+ *
+ * @description
+ * Componente que se encarga de formatear y renderizar un número
+ * como precio utilizando el objeto nativo Intl.NumberFormat de JavaScript.
+ * Esto asegura una correcta localización del formato de moneda.
+ *
+ * @example
+ * <Price value={1234.567} currenType="USD" countrysType="es-US" />
+ * Muestra $1,234.57
+ *
+ * @example
+ * <Price value={1234.567} />
+ * Muestra $1,234.57 (usa valores por defecto MXN y es-MX)
+ */
 export function Price({
   value,
   currenType = "MXN",
