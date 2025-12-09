545087014335e5c509514e3b17004599b7f68483
marcos-gl <marcosgueloz@gmail.com>
2025-12-08 20:10:54 -0600

    fix(highlight): update highlight component

--- src/components/ui/highlight/index.tsx
+++ src/components/ui/highlight/index.tsx
@@ -1,18 +1,36 @@
-import { IconName } from "@/components/ui/icon";
+import { Icon, type IconName } from "@/components/ui/icon";
 
-
-export interface HighlightProps{
-    numberHighlight: number,
-    textHighlight: string,
-    iconText?: IconName,
+export interface HighlightProps {
+  numberHighlight: number;
+  textHighlight: string;
+  iconText?: IconName;
 }
 
-export function Highlight({ numberHighlight, textHighlight, iconText }: HighlightProps) {
+/**
+ * Muestra un dato destacado, como una característica de una propiedad.
+ * Puede incluir un número, un texto descriptivo y un ícono opcional.
+ *
+ * @param {HighlightProps} props - Propiedades para el componente Highlight.
+ * @param {number} props.numberHighlight - El número a destacar.
+ * @param {string} props.textHighlight - El texto descriptivo.
+ * @param {IconName} [props.iconText] - El nombre del ícono a mostrar (opcional).
+ * @returns {JSX.Element} El componente de highlight.
+ *
+ * @example
+ * <Highlight numberHighlight={3} textHighlight="Recámaras" iconText="HomeIcon" />
+ */
+export function Highlight({
+  numberHighlight,
+  textHighlight,
+  iconText,
+}: HighlightProps) {
   return (
     <div className="flex flex-col items-center ">
-      <h1 className="font-bold text-brand-primary-500 text-xl">{numberHighlight}</h1>
-      <h1>{iconText}</h1>
-      <p className="neutral-base-900">{textHighlight}</p>
+      <span className="font-bold text-brand-primary-500 text-xl">
+        {numberHighlight}
+      </span>
+      {iconText && <Icon name={iconText} />}
+      <p className="text-neutral-base-900">{textHighlight}</p>
     </div>
   );
 }
