e4b603899e532d10ff6adf157fd51f4e7225dc7f
marcos-gl <marcosgueloz@gmail.com>
2025-12-09 18:35:48 -0600

    refactor(property-card): added props

--- src/components/ui/propertyCard/index.tsx
+++ src/components/ui/propertyCard/index.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
 
-export function PropertyCard() {
+type PropertyCardProps = {
+  imageUrl: string;
+  imageAlt: string;
+};
+
+/**
+ * Muestra una tarjeta con la información principal de una propiedad.
+ *
+ * @param imageUrl La URL de la imagen de la propiedad.
+ * @param imageAlt El texto alternativo para la imagen.
+ *
+ * @example
+ * <PropertyCard
+ *   imageUrl="/images/terreno.png"
+ *   imageAlt="Casa Residencial Moderna con Jardín"
+ * />
+ */
+export function PropertyCard({ imageUrl, imageAlt }: PropertyCardProps) {
   return (
     <div className="w-87 h-183 rounded-3xl shadow-md overflow-hidden">
       <div className="relative h-68 ">
-        <Image src="/images/terreno.png" alt="Casa Residencial Moderna con Jardín" fill={true}  className="z-0"/>
+        <Image src={imageUrl} alt={imageAlt} fill={true} className="z-0" />
       </div>
     </div>
   );
